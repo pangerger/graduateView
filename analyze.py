8d0d1699990024bde25d3f43604a544a502347c1
hi <996770483@qq.com>
2019-04-28 09:50:37 +0800

    first time

--- analyze.py
+++ analyze.py
@@ -6,11 +6,12 @@ from __future__ import unicode_literals
 
 import random
 import datetime
+import pymysql as MYSQLdb # 起个别名
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask.templating import Environment
 
-from pyecharts import HeatMap, Map,Bar
+from pyecharts import HeatMap, Map,Bar,Page
 from pyecharts.engine import ECHAERTS_TEMPLATE_FUNCTIONS
 from pyecharts.conf import PyEchartsConfig
 
@@ -29,12 +30,75 @@ class MyFlask(Flask):
     jinja_environment = FlaskEchartsEnvironment
 
 
+# connect to mysql
+def getcur():
+    try:
+        host='localhost'
+        passwd='root'
+        conn = MYSQLdb.connect(host=host, user='root', passwd=passwd, db='mfw', port=3306, charset='utf8')
+        cur = conn.cursor()
+        return cur
+        #cur_update = conn.cursor() #这个游标用于更新
+        #count = cur.execute(url_query)# 执行查询操作
+    except MYSQLdb.Error as e:
+        print(e.args)
+
+# close to mysql
+# def removecur(conn,cur):
+#     conn.close()
+#     cur.close()
+
 app = MyFlask(__name__)
 
 @app.route("/hi")
 @app.route("/")
 def index():
     return render_template('index.html')
+
+# city in top!
+@app.route("/top",methods=['post','get'])
+def top():
+    try:
+        topCityStart=request.form['topCityStart']
+        topCityEnd=request.form['topCityEnd']
+    except Exception as e:
+        print(e.args)
+        topCityStart='1'
+        topCityEnd='10'
+    start=topCityStart
+    end=topCityEnd
+    x=int(topCityEnd)
+    y=int(topCityStart)-1
+    z=x-y
+    topCityStart=str(y)
+    topCityEnd=str(z)
+    #topCityEnd=str(z)
+    
+    query="SELECT * FROM city order by nums desc limit "+topCityStart+","+topCityEnd
+    
+    cur=getcur()
+    count=cur.execute(query)
+    #count=int(count)
+    #pan=Page()
+    bar=Bar("top 旅游城市")
+    barH=Bar()
+    att=list()
+    value=list()
+    while count>0:
+        one=cur.fetchone()
+        att.append(one[1])
+        value.append(one[3])
+        #bar.add(one[1],int(one[3]))
+        #bar.add_yaxis(one[1],int(one[3]))
+        count=count-1
+
+    bar.add('出行人数',att,value,is_label_show = True)
+    
+    barH.add('出行人数',att,value,is_label_show = True,label_pos = 'inside',is_convert = True)
+
+    #cur.close()
+    #conn.close()
+    return render_template('city.html',micheal=bar,second=barH,topCityStart=start,topCityEnd=end,cityName=att)
 
 
 @app.route("/heatmap/")
@@ -59,6 +123,11 @@ def create_heatmap():
     return heatmap
 
 
+
+
+
+
+
 @app.route('/fujian/')
 def fujian():
     value = [20, 190, 253, 77, 65]
@@ -75,4 +144,5 @@ def bar():
     bar.add("范围", ["shop"], [120])
     return render_template('heatmap.html', m=bar)
 
-app.run(port=5000)
+
+app.run(port=5000,debug=True)
